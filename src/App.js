107794949bdbc313a496256472717ce6ab92aa4d
BosEriko <bos.eriko@gmail.com>
2019-10-26 22:35:36 +0800

    add types to buttons

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Output from './components/Output';
 class App extends React.Component {
   render() {
     const buttons = [
-      '1',      '2',      '3',      '+',
-      '4',      '5',      '6',      '-',
-      '7',      '8',      '9',      '*',
-      'Clear',  '0',      '=',      '/',
+      { data: '1', type: 'integer' },       { data: '2', type: 'integer' }, { data: '3', type: 'integer' },  { data: '+', type: 'function' },
+      { data: '4', type: 'integer' },       { data: '5', type: 'integer' }, { data: '6', type: 'integer' },  { data: '-', type: 'function' },
+      { data: '7', type: 'integer' },       { data: '8', type: 'integer' }, { data: '9', type: 'integer' },  { data: '*', type: 'function' },
+      { data: 'clear', type: 'function' },  { data: '0', type: 'integer' }, { data: '=', type: 'function' },  { data: '/', type: 'function' },
     ];
     return (
       <div className="App">
         <div className="calculator">
           <Output />
-          { buttons.map((char, key) => <Button char={char} key={key} />) }
+          { buttons.map((button, key) => <Button char={button.data} type={button.type} key={key} />) }
         </div>
       </div>
     );
