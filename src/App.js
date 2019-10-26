2628c0e5b07b6f4c07b34e86948a81122ca64e12
BosEriko <bos.eriko@gmail.com>
2019-10-26 22:37:03 +0800

    rename data to char

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import Output from './components/Output';
 class App extends React.Component {
   render() {
     const buttons = [
-      { data: '1', type: 'integer' },       { data: '2', type: 'integer' }, { data: '3', type: 'integer' },  { data: '+', type: 'function' },
-      { data: '4', type: 'integer' },       { data: '5', type: 'integer' }, { data: '6', type: 'integer' },  { data: '-', type: 'function' },
-      { data: '7', type: 'integer' },       { data: '8', type: 'integer' }, { data: '9', type: 'integer' },  { data: '*', type: 'function' },
-      { data: 'clear', type: 'function' },  { data: '0', type: 'integer' }, { data: '=', type: 'function' },  { data: '/', type: 'function' },
+      { char: '1', type: 'integer' },       { char: '2', type: 'integer' }, { char: '3', type: 'integer' },  { char: '+', type: 'function' },
+      { char: '4', type: 'integer' },       { char: '5', type: 'integer' }, { char: '6', type: 'integer' },  { char: '-', type: 'function' },
+      { char: '7', type: 'integer' },       { char: '8', type: 'integer' }, { char: '9', type: 'integer' },  { char: '*', type: 'function' },
+      { char: 'clear', type: 'function' },  { char: '0', type: 'integer' }, { char: '=', type: 'function' },  { char: '/', type: 'function' },
     ];
     return (
       <div className="App">
         <div className="calculator">
           <Output />
-          { buttons.map((button, key) => <Button char={button.data} type={button.type} key={key} />) }
+          { buttons.map((button, key) => <Button char={button.char} type={button.type} key={key} />) }
         </div>
       </div>
     );
