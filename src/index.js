54e50bad9b9a45c23f24511697d4c22cd96c66e1
BosEriko <bos.eriko@gmail.com>
2019-10-26 13:07:33 +0800

    basic template

--- src/index.js
+++ src/index.js
@@ -4,14 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { Provider } from 'react-redux';
-import { store } from './redux';
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+  <App />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
