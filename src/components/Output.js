933b01778843d562ff43dc28d7d21f9394d62f65
BosEriko <bos.eriko@gmail.com>
2019-10-27 13:02:43 +0800

    finish goal 1

--- src/components/Output.js
+++ src/components/Output.js
@@ -6,15 +6,29 @@ import { connect } from 'react-redux';
 class Output extends React.Component {
   render() {
     return (
-      <textarea rows="1" className="output" defaultValue={this.props.currentData.length > 0 ? this.props.currentData[this.props.currentData.length - 1] : 0} />
+      <textarea rows="1" className="output" defaultValue={this.props.currentData} />
     );
   }
 }
 
 // Container
-const mapStateToProps = state => ({
-  currentData: state.currentData
-});
+const mapStateToProps = state => {
+  if (state.currentData.length > 0) {
+    if (isNaN(state.currentData[state.currentData.length - 1])) {
+      return {
+        currentData: state.currentData[state.currentData.length - 2]
+      }
+    } else {
+      return {
+        currentData: state.currentData[state.currentData.length - 1]
+      }
+    }
+  } else {
+    return {
+      currentData: 0
+    }
+  }
+};
 
 const mapDispatchToProps = {};
 
