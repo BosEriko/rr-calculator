bc2811370fdfdd4643831459e273f96b612c842d
BosEriko <bos.eriko@gmail.com>
2019-10-27 17:26:40 +0800

    toggle scientific mode now working

--- src/App.js
+++ src/App.js
@@ -11,13 +11,16 @@ import {
 
 class App extends React.Component {
   render() {
-    const buttons = [
+    const normalButtons = [
       { char: '1', type: 'integer' },       { char: '2', type: 'integer' },   { char: '3', type: 'integer' },     { char: '+', type: 'arithmetic' },
       { char: '4', type: 'integer' },       { char: '5', type: 'integer' },   { char: '6', type: 'integer' },     { char: '-', type: 'arithmetic' },
       { char: '7', type: 'integer' },       { char: '8', type: 'integer' },   { char: '9', type: 'integer' },     { char: '*', type: 'arithmetic' },
       { char: 'AC', type: 'clear' },        { char: '0', type: 'integer' },   { char: '=', type: 'result' },      { char: '/', type: 'arithmetic' },
-      { char: 'Sign', type: 'sign' },       { char: 'Sqr', type: 'square' },  { char: 'Root', type: 'root' },
     ];
+    const scientificButtons = normalButtons.concat([
+      { char: 'Sign', type: 'sign' }, { char: 'Sqr', type: 'square' }, { char: 'Root', type: 'root' },
+    ]);
+    const buttons = this.props.scientificMode ? scientificButtons : normalButtons;
     return (
       <div className="App">
         <div className="toggle-buttons">
